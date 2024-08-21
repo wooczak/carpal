277556d0dcb8df15aae6b059dd18bef8324acf42
wooczak <kacper.luczak99@gmail.com>
2024-08-21 19:16:03 +0200

    Styling Sign In page

--- dashboard/app/sign-in.tsx
+++ dashboard/app/sign-in.tsx
@@ -1,7 +1,14 @@
 import { Link, router } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function SignIn() {
   const [email, setEmail] = useState("");
@@ -32,25 +39,109 @@ export default function SignIn() {
 
   return (
     <View style={styles.container}>
-      <Text>SignIn</Text>
-      <TextInput
-        autoCapitalize="none"
-        onChangeText={setEmail}
-        placeholder="email"
-      />
-      <TextInput placeholder="password" onChangeText={setPassword} />
-      <Button title="Sign in" onPress={handleSignIn} />
-      <Link href="/sign-up">Sign up</Link>
+      <Text style={styles.heading}>Welcome to CarPal</Text>
+      <Text style={styles.subheading}>Sign in to your account</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          onChangeText={setEmail}
+          placeholder="email"
+        />
+        <TextInput
+          secureTextEntry
+          style={styles.input}
+          placeholder="password"
+          onChangeText={setPassword}
+        />
+        <Pressable style={styles.cta} onPress={handleSignIn}>
+          <Text style={styles.ctaText}>SIGN IN</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.signUpCTAWrapper}>
+        <Text>Don't have an account yet?</Text>
+        <Pressable style={styles.signUpCTA} onPress={handleSignIn}>
+          <Link href="/sign-up">
+            <Text style={styles.signUpCTAText}>SIGN UP</Text>
+          </Link>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const lightYellow = "#FEFBD8";
+const mainPeach = "#EECEB9";
+const lightPurple = "#BB9AB1";
+const mainPurple = "#987D9A";
+
 const styles = StyleSheet.create({
   container: {
+    width: "100%",
+    height: "100%",
     flex: 1,
+    padding: 16,
     backgroundColor: "#fff",
-    alignItems: "center",
+    alignItems: "flex-start",
     justifyContent: "center",
   },
+  heading: {
+    fontSize: 24,
+    fontWeight: "700",
+    opacity: 0.5,
+    color: mainPurple,
+  },
+  subheading: {
+    fontSize: 42,
+    fontWeight: "800",
+    lineHeight: 36,
+    paddingTop: 8,
+    paddingBottom: 8,
+  },
+  inputWrapper: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    gap: 8,
+  },
+  input: {
+    borderColor: mainPurple,
+    borderWidth: 1,
+    width: "100%",
+    paddingHorizontal: 8,
+    paddingVertical: 12,
+    borderRadius: 8,
+  },
+  cta: {
+    width: "100%",
+    backgroundColor: mainPurple,
+    padding: 16,
+    borderRadius: 8,
+  },
+  ctaText: {
+    textAlign: "center",
+    color: "#FFF",
+    fontWeight: "600",
+    letterSpacing: 1
+  },
+  signUpCTAWrapper: {
+    width: "100%",
+    marginTop: 32,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    gap: 8
+  },
+  signUpCTA: {
+    borderWidth: 1,
+    width: "100%",
+    padding: 12,
+    borderRadius: 8
+  },
+  signUpCTAText: {
+    textAlign: "center",
+    fontWeight: "600",
+  }
 });
